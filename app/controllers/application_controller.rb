05040a10329d3b948265f5f352e0027e343a2c30
n-haruhi <hr23nakanishi@gmail.com>
2024-02-05 12:01:26 +0000

    [Update]deviseのinstall

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # protectedは呼び出された他のコントローラからも参照することができる
+  protected
+
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
