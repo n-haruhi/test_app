c78add1daf5471a1e35155cb6d162bc5fb58c558
n-haruhi <hr23nakanishi@gmail.com>
2024-02-06 14:33:40 +0000

    [Update]アソシエーションとコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
 
   devise_for :users
+  # ルーティングを一括で。onlyオプションで限定して生成
+  resources :post_images, only: [:new, :index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
