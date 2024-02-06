c420b4bde81298acb7b236428f7ae4e216cc4489
n-haruhi <hr23nakanishi@gmail.com>
2024-02-06 14:27:42 +0000

    [Update]sign in, sign out後の設定、モデル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Deviseが用意しているメソッド。サインイン後にどこに遷移するかを設定
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   # protectedは呼び出された他のコントローラからも参照することができる
   protected
 
