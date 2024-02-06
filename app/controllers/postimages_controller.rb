792a25a40515b7f619e45dfe421434ea287aa0a8
n-haruhi <hr23nakanishi@gmail.com>
2024-02-06 14:51:43 +0000

    [Update]投稿作成機能

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -1,5 +1,17 @@
 class PostimagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to post_images_path, notice: "投稿成功ました。"
+    else
+      render :new
+    end
   end
 
   def index
@@ -7,4 +19,12 @@ class PostimagesController < ApplicationController
 
   def show
   end
+
+  # 投稿のストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
